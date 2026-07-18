14403e4eaee9255168709e12b86151880948e78b
Keinsleif <mountaindull@gmail.com>
2026-07-19 02:24:07 +0900

    fix: test

--- sbsp_backend/src/lib.rs
+++ sbsp_backend/src/lib.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Elastic-2.0
 // Copyright (c) 2025 Keinsleif (https://github.com/Keinsleif)
 
+// test
+
 use serde::{Deserialize, Serialize};
 #[cfg(feature = "backend")]
 use tokio::sync::{broadcast, mpsc, oneshot, watch};
